7eacae566019f2735f2d7b7e6e7caf71dbea0de3
rahuls1999 <63736373+rahuls1999@users.noreply.github.com>
2020-05-14 09:12:09 +0530

    Update and rename Costfunction.m to CH5019_GRP28_Costfunction.m

--- Question_2/CH5019_GRP28_Costfunction.m
+++ Question_2/CH5019_GRP28_Costfunction.m
@@ -1,12 +1,12 @@
 % Function to compute cost and gradient 
-function [J, grad] = Costfunction(theta, X, y)
+function [J, grad] = CH5019_GRP28_Costfunction(theta, X, y)
 
 m = length(y);
 
 J = 0;
 grad = zeros(size(theta));
 
-h = sigmoid(X*theta);
+h = CH5019_GRP28_sigmoid(X*theta);
 J = -(1/m)*sum(y'*log(h)+(1-y)'*log(1-h));
 grad = (X'*(h-y))./m;
     
